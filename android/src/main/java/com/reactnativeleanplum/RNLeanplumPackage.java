dcad88376f2017fba69911d1fa055fd82d5e3728
damien <dd@gleeph.net>
2019-04-29 15:34:35 +0200

    fix(android): forgotten imports

--- android/src/main/java/com/reactnativeleanplum/RNLeanplumPackage.java
+++ android/src/main/java/com/reactnativeleanplum/RNLeanplumPackage.java
@@ -10,21 +10,15 @@ import com.facebook.react.uimanager.ViewManager;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import android.os.Build;
+import android.app.NotificationManager;
+import android.app.NotificationChannel;
 
 public class RNLeanplumPackage implements ReactPackage {
     private Application application;
 
     public RNLeanplumPackage(Application app) {
         application = app;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "default";
-            String description = "default channel to send message";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-    }
 
     }
 
@@ -33,9 +27,23 @@ public class RNLeanplumPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
+    private void initialiseChannels(ReactApplicationContext c){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "default";
+            String description = "default channel to send message";
+            String CHANNEL_ID="0";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = c.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
+        initialiseChannels(reactContext);
 
         modules.add(new RNLeanplum(reactContext, application));
         modules.add(new RNLPInbox(reactContext, application));
