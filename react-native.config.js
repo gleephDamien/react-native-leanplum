1158f242c87fa109d7f0ac4dee5f953fed79e1c0
damien <dd@gleeph.net>
2019-10-09 15:04:45 +0200

    3.0.0-beta.8

--- react-native.config.js
+++ react-native.config.js
@@ -1,6 +1,15 @@
+
+const path = require('path');
 module.exports = {
-  project: {
-    ios: {},
-    android: {}, 
-  },
+    dependency: {
+       platforms: {
+         android:{
+           packageInstance:"new RNLeanplumPackage(MainApplication.this)",
+         },
+         ios: {
+
+             podspecPath:path.join(__dirname, 'react-native-leanplum.podspec')
+         }
+       }
+    },
 };
